4a136c694eb6d344712ce96e0468a21db0771e45
ecristal <e.cristaldo27@gmail.com>
2020-04-25 14:47:24 -0400

    primer commit

---
+++ lavamanosMainWindow.py
@@ -0,0 +1,71 @@
+from PyQt5 import uic, QtCore, QtGui, QtWidgets, QtMultimedia, QtMultimediaWidgets
+import RPi.GPIO as GPIO
+from widgetDeTexto import widgetDeTexto
+
+mainWindow = uic.loadUiType("UI/mainwindow.ui")[0]
+
+class lavamanosMainWindow(QtWidgets.QMainWindow, mainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        self.mediaPlayer = QtMultimedia.QMediaPlayer(None,QtMultimedia.QMediaPlayer.VideoSurface)
+        self.videoWidget = QtMultimediaWidgets.QVideoWidget()
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.videoWidget)
+        self.videoFrame.setLayout(layout)
+
+        self.listaDeVideos = []
+        self.modeloListaDeVideos = QtGui.QStandardItemModel(self.videoListView)
+
+        self.bAgregarVideo.clicked.connect(self.bAgregarVideoClicked)
+        self.bPlay.clicked.connect(self.bPlayPressed)
+        self.bPausa.clicked.connect(self.bPausaPressed)
+        self.bStop.clicked.connect(self.bStopPressed)
+        self.bIniciarPrograma.clicked.connect(self.bIniciarProgramaPressed)
+
+    def bAgregarVideoClicked(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        files, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"Agregar Videos", "","All Files (*)", options=options)
+        for i in range(0,len(files)):
+            self.listaDeVideos.append(files[i])
+            item = QtGui.QStandardItem(files[i])
+            self.modeloListaDeVideos.appendRow(item)
+        self.videoListView.setModel(self.modeloListaDeVideos)
+
+    def bPlayPressed(self):
+        indiceVideo = self.videoListView.currentIndex()
+        direccionVideo = indiceVideo.data(QtCore.Qt.DisplayRole)
+        print(direccionVideo)
+        self.mediaPlayer.setMedia(QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(direccionVideo)))
+        self.mediaPlayer.play()
+
+    def bStopPressed(self):
+        self.mediaPlayer.stop()
+
+    def bPausaPressed(self):
+        self.mediaPlayer.pause()
+
+    def bIniciarProgramaPressed(self):
+        print('bIniciarProgramaPressed')
+        dialogDeMensajes = widgetDeTexto()
+        dialogDeMensajes.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowType_Mask)
+        dialogDeMensajes.showFullScreen()
+        dialogDeMensajes.setListaDeReproduccion(self.listaDeVideos)
+        dialogDeMensajes.exec_()
+        GPIO.cleanup()
+    #def keyPressEvent(self, event):
+    #    if (event.key() == QtCore.Qt.Key_Escape) and (self.videoWidget.isFullScreen() == False):
+    #        print('HOLA')
+    #        self.videoWidget.setFullScreen(False)
+    #        event.accept()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = lavamanosMainWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ widgetDeTexto.py
@@ -0,0 +1,45 @@
+from PyQt5 import uic, QtCore, QtGui, QtWidgets, QtMultimedia, QtMultimediaWidgets
+import RPi.GPIO as GPIO
+
+widget_ui_ = uic.loadUiType("UI/widgetDeTexto.ui")[0]
+
+class widgetDeTexto(QtWidgets.QDialog, widget_ui_):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        self.mediaPlayer = QtMultimedia.QMediaPlayer(None,QtMultimedia.QMediaPlayer.VideoSurface)
+        self.videoWidget = QtMultimediaWidgets.QVideoWidget()
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+
+        self.playlist = QtMultimedia.QMediaPlaylist()
+
+        self.listaDeReproduccion = []
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.videoWidget)
+        self.setLayout(layout)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Canilla
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Jabon
+
+        def gpio23_Interrupted(canal):
+            print('Interrupcion en el pin 23')
+            for i in reversed(range(self.layout().count())):
+                self.layout().itemAt(i).widget().deleteLater()
+
+        def gpio24_Interrupted(canal):
+            print('Interrupcion en el pin 24')
+            self.mediaPlayer.stop()
+
+        GPIO.add_event_detect(23, GPIO.FALLING, callback=gpio23_Interrupted, bouncetime=300)
+        GPIO.add_event_detect(24, GPIO.FALLING, callback=gpio24_Interrupted, bouncetime=300)
+
+    def setListaDeReproduccion(self,listaDeReproduccion):
+        self.listaDeReproduccion = listaDeReproduccion
+        for video in self.listaDeReproduccion:
+            self.playlist.addMedia(QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(video)))
+        self.playlist.setCurrentIndex(1)
+        self.mediaPlayer.setPlaylist(self.playlist)
+        self.mediaPlayer.play()
