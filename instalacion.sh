e9799c20cf3e4cf6e5dedd3ab5da74e5a1864c35
Esteban Cristaldo <e.cristaldo27@gmail.com>
2020-05-30 18:45:21 -0400

    se agregan qtmultimedia a la instalacion

--- instalacion.sh
+++ instalacion.sh
@@ -1,2 +1,4 @@
 sudo apt-get update 
-sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools
+sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools -y
+sudo apt-get install python3-pyqt5.qtmultimedia -y
+sudo apt-get zerotier-cli -y 
